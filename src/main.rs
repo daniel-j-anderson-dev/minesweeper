77316d1782e227874d54b484eb5cb3e206342ffd
Michael Dodson <MichaelJDodson42@gmail.com>
2024-02-28 22:39:29 -0500

    Board has generic size and is composed of cells. Implemented display for Board and Cell.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,49 @@
 use rand::Rng;
-use std::io;
+use std::{fmt::Display, io};
 fn main() {
    
     let minesweeper_board = Board::initialize_random();
-    minesweeper_board.display();
+    println!("{}", minesweeper_board);
     
 
 }
 
-enum Cell {
-    True,
-    False,
-    None,
+#[derive(Clone, Copy)]
+/// contains the information pertaining to a cell
+struct Cell {
+    is_mine: bool,
+    /// number of mines surrounding this cell
+    local_mines: usize,
 }
-struct Board {
-    pub cells: [[bool; 5]; 5],
+impl Display for Cell {
+    /// display as mine as "*". If empty, displays the number of mines in cells around it
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.is_mine {
+            write!(f, "*")?;
+        }else {
+            write!(f, "{}", self.local_mines)?;
+        }
+        return Ok(());
+    }
 }
-impl std::fmt::Display for Board {
+impl Cell {
+    /// a clear cell used for a default value
+    pub const CLEAR: Self = Self {
+        is_mine: false,
+        local_mines: 0,
+    };
+    /// used to generate a random cell
+    pub fn random() -> Self {
+        let mut rng = rand::thread_rng(); // random thread value
+        return Self { is_mine: rng.gen_bool(0.5), local_mines: 0 };
+    }
+}
+/// Board contains an 2D array of cells
+struct Board<const WIDTH: usize, const HEIGHT: usize> {
+    pub cells: [[Cell; WIDTH]; HEIGHT],
+}
+impl<const W: usize, const H: usize> Display for Board<W, H> {
+    /// displays a board
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for &row in self.cells.iter() {
             for &element in row.iter() {
@@ -27,19 +54,19 @@ impl std::fmt::Display for Board {
         return Ok(());
     }
 }
-impl Board {
+impl<const W: usize, const H: usize> Board<W, H> {
     /// Initialize the minesweeper board with random true/false
     pub fn initialize_random() -> Self {
-        let mut rng = rand::thread_rng(); // random thread value
-        let mut new_board = [[true;5] ;5];
+        let mut cells = [[Cell::CLEAR; W] ; H];
         
-        for &row in new_board.iter() {
+        for row in cells.iter_mut() {
             for element in row.iter_mut() {
-                *element = rng.gen_bool(0.5);
+                *element = Cell::random();
             }
         }
 
-
-        todo!()
+        return Self {
+            cells: cells,
+        };
     }
 }
