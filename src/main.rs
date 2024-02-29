d9ac640798bad419aaf75a6cc3c885d43856d7b8
Michael Dodson <MichaelJDodson42@gmail.com>
2024-02-28 22:04:37 -0500

    Refactored functions as a method and trait implementation.

--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,54 @@ use rand::Rng;
 use std::io;
 fn main() {
    
-    let minesweeper_board = initialize_random_board();
-    display_board(&minesweeper_board);
-
-}
-
-struct Board {
-    positions: [[bool; 5]; 5],
-}
-
-// outputs the minesweeper board
-fn display_board(latest_board: &Board) {
-    for &row in latest_board.positions.iter() {
-        for &element in row.iter() {
-            print!("{} ", element)
-        }
-        println!();
-    }
-}
-
-// Initialize the minesweeper board with random true/false
-fn initialize_random_board() -> Board {
-    let mut rng = rand::thread_rng(); // random thread value
-    let mut new_board = [[true;5] ;5];
+    let minesweeper_board = Board::initialize_random();
+    minesweeper_board.display();
     
-    for &row in new_board.iter() {
-        for element in row.iter_mut() {
-            element = rng.gen_bool(0.5);
+
+}
+
+enum Cell {
+    True,
+    False,
+    None,
+}
+struct Board {
+    pub cells: [[bool; 5]; 5],
+}
+impl std::fmt::Display for Board {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for &row in self.cells.iter() {
+            for &element in row.iter() {
+                write!(f, "{} ", element)?;
+            }
+            write!(f, "\n")?;
+        }
+        return Ok(());
+    }
+}
+impl Board {
+    /// Initialize the minesweeper board with random true/false
+    pub fn initialize_random() -> Self {
+        let mut rng = rand::thread_rng(); // random thread value
+        let mut new_board = [[true;5] ;5];
+        
+        for &row in new_board.iter() {
+            for element in row.iter_mut() {
+                *element = rng.gen_bool(0.5);
+            }
+        }
+
+
+        todo!()
+    }
+
+    /// outputs the minesweeper board
+    pub fn display(&self) {
+        for &row in self.cells.iter() {
+            for &element in row.iter() {
+                print!("{} ", element)
+            }
+            println!();
         }
     }
-    let new_board = Board { positions: [[rng.gen_bool(0.5);5] ;5] }; // create board at random
-    return new_board;
 }
