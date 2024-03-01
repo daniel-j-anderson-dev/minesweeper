1211f4b0db267b0c26a0710b5d2300bca35a161a
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-03-01 14:00:41 -0500

    Added function to get line of input from terminal

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,28 @@ fn main() {
     println!("{}", minesweeper_board);
 }
 
+/// This function will display the `prompt` to the user using `standard output stream` ([std::io::Stdout]). <br>
+/// A line of input is read from `standard input stream` ([std::io::Stdin]) and returned.
+/// - Trailing and leading whitespace is trimmed from the input line.
+/// # Errors
+/// - If cannot write `prompt` to `standard output stream`
+/// - If cannot flush `standard output stream`
+/// - If cannot read input from `standard input stream`
+pub fn get_input(prompt: &str) -> Result<String, std::io::Error> {
+    use std::io::{stdin, stdout, Write};
+
+    // prompt the user
+    stdout().write(prompt.as_bytes())?; // write the prompt to `stdout`
+    stdout().flush()?; // flush the standard output stream (ensure all data reaches its destination ie the terminal)
+
+    // read a line of input
+    let mut input = String::new(); // create a [String] to hold user input
+    stdin().read_line(&mut input)?; // read a line from `stdin` into `input`
+    let input = input.trim().to_string(); // shadow `input` with a clone that doesn't include leading or trailing whitespace
+
+    return Ok(input);
+}
+
 #[derive(Clone, Copy)]
 /// contains the information pertaining to a cell
 struct Cell {
