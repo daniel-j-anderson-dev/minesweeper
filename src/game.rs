5c272876ed734c569549f7da0b123f713639b917
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-03-02 12:11:31 -0500

    Added macroquad support

--- src/game.rs
+++ src/game.rs
@@ -1,20 +1,20 @@
 use std::io::{stdout, Write};
 
-use crate::{
-    board::Board,
-    input::{clear_terminal, get_parsed_input, Action},
-};
+use macroquad::{miniquad::window::screen_size, prelude::*};
 
-pub const WIDTH: usize = 10;
-pub const HEIGHT: usize = 10;
+use crate::{board::Board, input::{clear_terminal, get_parsed_input, Action}};
+
+pub const WIDTH: usize = 32;
+pub const HEIGHT: usize = WIDTH;
 
 pub const EASY_MINES: f64 = 0.0625;
 pub const MEDIUM_MINES: f64 = 0.125;
 pub const HARD_MINES: f64 = 0.25;
 
+#[derive(Debug, Clone, Copy)]
 pub struct Game {
     board: Board<WIDTH, HEIGHT>,
-    is_game_over: bool,
+    state: GameState,
     cell_index: (usize, usize),
     action: Action,
 }
@@ -22,29 +22,35 @@ impl Game {
     pub fn new() -> Self {
         return Self {
             board: Board::random(MEDIUM_MINES),
-            is_game_over: false,
+            state: GameState::Playing,
             cell_index: (0, 0),
             action: Action::Cancel,
         };
     }
 
     pub fn execute_turn(&mut self) -> Result<GameState, std::io::Error> {
-        self.display_board()?;
-        self.get_cell_index()?;
-        self.get_action()?;
+        self.handle_input();
         self.execute_action()?;
-        return self.handle_game_over();
+        return Ok(self.state);
+    }
+
+    pub fn execute_terminal_turn(&mut self) -> Result<GameState, std::io::Error> {
+        self.display_board_terminal()?;
+        self.get_cell_index_terminal()?;
+        self.get_action_terminal()?;
+        self.execute_action()?;
+        return Ok(self.state);
     }
 
     /// Only to be called in execute_turn. must be called first
-    fn display_board(&self) -> Result<(), std::io::Error> {
+    fn display_board_terminal(&self) -> Result<(), std::io::Error> {
         clear_terminal()?;
         writeln!(stdout(), "{}\n", self.board)?;
         return Ok(());
     }
 
     /// Only to be called in execute_turn. must be called second
-    fn get_cell_index(&mut self) -> Result<(), std::io::Error> {
+    fn get_cell_index_terminal(&mut self) -> Result<(), std::io::Error> {
         loop {
             // allow user to select a cell
             let row_index = get_parsed_input("Select a cell\nPlease enter a row number: ")?;
@@ -65,11 +71,12 @@ impl Game {
     }
 
     /// Only to be called in execute_turn. must be called third
-    pub fn get_action(&mut self) -> Result<(), std::io::Error> {
+    pub fn get_action_terminal(&mut self) -> Result<(), std::io::Error> {
         self.action =
             get_parsed_input("\nSelect an action for this cell\nReveal\nFlag\nUnflag\nCancel\n")?;
         return Ok(());
     }
+
 
     /// Only to be called in execute_turn. must be called fourth
     pub fn execute_action(&mut self) -> Result<(), std::io::Error> {
@@ -77,7 +84,7 @@ impl Game {
             Action::Reveal => {
                 self.board[self.cell_index].reveal();
                 if self.board[self.cell_index].is_mine() {
-                    self.is_game_over = true;
+                    self.state = GameState::GameOver;
                 }
             }
             Action::Flag => self.board[self.cell_index].flag(),
@@ -87,26 +94,68 @@ impl Game {
         return Ok(());
     }
 
-    /// Only to be called in execute_turn. must be called last
-    pub fn handle_game_over(&mut self) -> Result<GameState, std::io::Error> {
-        return if self.action.is_reveal() && self.board[self.cell_index].is_mine() {
-            clear_terminal()?;
-            writeln!(stdout(), "\nYou revealed a mine!\nGAME OVER\n{}", self.board.clone_revealed())?;
-            Ok(GameState::GameOver)
-        } else {
-            Ok(GameState::Playing)
-        };
+    pub fn set_action(&mut self, action: Action) {
+        self.action = action;
+    }
+
+    pub fn set_cell_index(&mut self, cell_index: (usize, usize)) {
+        self.cell_index = cell_index;
+    }
+
+    pub fn draw(&mut self) {
+        let size: f32 = screen_width().min(screen_height()) * 0.027;
+        let screen_origin = Vec2::from(screen_size()) / 2.0;
+        let board_position = screen_origin - (Vec2::new(WIDTH as f32, HEIGHT as f32) * size) / 2.0;
+        let border_size = size / 10.0;
+
+        for row_index in 0..HEIGHT {
+            for column_index in 0..WIDTH {
+                let cell_position = Vec2::new(size * row_index as f32, size * column_index as f32) + board_position;
+                
+                let cell_boundary = Rect::new(cell_position.x, cell_position.y, size, size);
+                
+                // draw border
+                draw_rectangle(
+                    cell_boundary.x,
+                    cell_boundary.y,
+                    cell_boundary.w,
+                    cell_boundary.h,
+                    GRAY
+                );
+    
+                // draw interior
+                draw_rectangle(
+                    cell_boundary.x + border_size,
+                    cell_boundary.y + border_size,
+                    cell_boundary.w - 2.0 * border_size,
+                    cell_boundary.h - 2.0 * border_size,
+                    LIGHTGRAY
+                );
+            }
+        }
+    }
+    
+    fn handle_input(&mut self) {
     }
 }
 
 /// A marker type to signify a game over
+#[derive(Debug, Clone, Copy)]
 pub enum GameState {
     GameOver,
     Playing,
+    Quit,
 }
 impl GameState {
     pub fn is_game_over(&self) -> bool {
         return if let GameState::GameOver = self {
+            true
+        } else {
+            false
+        };
+    }
+    pub fn is_quit(&self) -> bool {
+        return if let GameState::Quit = self {
             true
         } else {
             false
